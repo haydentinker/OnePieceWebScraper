0644fc59ec54eb4715b6c87b51f76f5a6d92a494
haydentinker <haydentinker613@gmail.com>
2023-05-06 17:44:15 -0700

    Improving scrapy scraper

--- one_piece_scraper/one_piece_scraper/spiders/character_spider.py
+++ one_piece_scraper/one_piece_scraper/spiders/character_spider.py
@@ -7,6 +7,15 @@ class CharacterSpiderSpider(scrapy.Spider):
     start_urls = ["https://onepiece.fandom.com/wiki/List_of_Canon_Characters"]
 
     def parse(self, response):
-        table_rows = response.css("table tr")
-        for row in table_rows:
-            print(table_rows.css("td a ::attr(href)").get())
+        tables = response.xpath('//table[@class="wikitable sortable"]')
+        for table in tables:
+            rows = table.xpath(".//tr")
+            for row in rows:
+                tds = row.xpath(".//a")
+                next_page_url = "https://onepiece.fandom.com/" + tds.attrib["href"]
+                print(next_page_url)
+                if "character" not in next_page_url:
+                    yield response.follow(next_page_url, callback=self.character_parse)
+
+    def character_parse(self, response):
+        print(response.css("aside").get())
