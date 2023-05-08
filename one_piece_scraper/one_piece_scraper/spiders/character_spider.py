238bf6ed15c856de6e81b75a1133c23458daf84c
haydentinker <haydentinker613@gmail.com>
2023-05-08 11:05:01 -0700

    Adding more columns to csv
    Scraper can now get the anime_debut, affiliations, and occupations. Birthdays will be next.

--- one_piece_scraper/one_piece_scraper/spiders/character_spider.py
+++ one_piece_scraper/one_piece_scraper/spiders/character_spider.py
@@ -24,13 +24,33 @@ class CharacterSpiderSpider(scrapy.Spider):
     def character_parse(self, response):
         # print("hey")
         # print(response.xpath('//div[@class="pi-data-value"]').get())
+        url_div = response.xpath("//figure[@class='pi-item pi-image']/a/@href").get()
 
         name_div = response.xpath(
             "//div[@class='pi-item pi-data pi-item-spacing pi-border-color']/div[@class='pi-data-value pi-font']"
         )[2].get()
-        selector = Selector(text=name_div)
+        name_selector = Selector(text=name_div)
+        anime_debut = response.xpath(
+            "//div[@class='pi-item pi-data pi-item-spacing pi-border-color']/div[@class='pi-data-value pi-font']"
+        )[3].get()
+        anime_selector = Selector(text=anime_debut)
+        affiliations = response.xpath(
+            "//div[@class='pi-item pi-data pi-item-spacing pi-border-color']/div[@class='pi-data-value pi-font']"
+        )[4].get()
+        affiliations_selector = Selector(text=affiliations)
+        occupations = response.xpath(
+            "//div[@class='pi-item pi-data pi-item-spacing pi-border-color']/div[@class='pi-data-value pi-font']"
+        )[5].get()
+        occupations_selector = Selector(text=occupations)
+        birthday = response.xpath('//div[@data-source="birth"]')
+        birth_elements = response.xpath('//div[@data-source="birth"]')
         yield {
-            "character": selector.xpath(
+            "url": url_div,
+            "character": name_selector.xpath(
                 '//div[@class="pi-data-value pi-font"]/text()'
-            ).get()
+            ).get(),
+            "anime_debut": anime_selector.xpath("//a/text()")[1].get(),
+            "affiliations": affiliations_selector.xpath("//@title").get(),
+            "occupations": occupations_selector.xpath("//@title").get(),
+            "birthday": birth_elements.get(),
         }
