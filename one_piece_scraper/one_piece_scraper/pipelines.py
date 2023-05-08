58144b572b45504b8bc52465a28a7a62d6b15806
haydentinker <haydentinker613@gmail.com>
2023-05-08 15:43:40 -0700

    Beginning to set up pipeline to clean data

--- one_piece_scraper/one_piece_scraper/pipelines.py
+++ one_piece_scraper/one_piece_scraper/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+import re
 
 
 class OnePieceScraperPipeline:
+    def __init__(self):
+        self.names_seen = set()
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        # Check Length of Item
+        if len(adapter.keys()) != 6:
+            raise DropItem(("Missing fields"))
+        # Check for duplicates
+        if adapter["character"] in self.names_seen:
+            raise DropItem("Duplicate character")
+        # url, character, anime_debut, affiliations, occupations, birthday
+        # Clean Name
+        name = adapter.get("character")
+        name = re.sub("\(.*?\)", "", name)
+        adapter["character"] = re.sub("\[.*?\]", "", name)
         return item
